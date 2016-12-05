727cca0fd9293b007d61b4c79d36db62ba7cca6a
Worker <aochen_lee@sina.com>
2016-12-05 15:38:24 +0800

    程序架构： 1、各部分的正确导入； 2、flask应用上下文的用法（注意email.py中send_email()方法中current_app()的用法）需要去理解一下flask.g和flask.current_app()的用法； 3、现程序python runserver和shell都没有问题，但无法测试（test），提示AssertionError:Popped wrong app context.无法修改，不明白到底问题是怎么回事。

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+from flask_wtf import Form
+from wtforms import SubmitField, StringField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
