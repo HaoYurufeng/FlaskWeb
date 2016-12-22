a9456c9bf234b5909c6e2523f7a568108f2fd219
Worker <aochen_lee@sina.com>
2016-12-22 16:53:31 +0800

    问题： ***<1>数据库：注册登录的用户权限正常，但在通过数据库手动添加的Admin用户查看时，只有Admin用户，即注册登录的用户的权限也显示为Admin;      在手动在数据库中加入User用户角色后，通过Admin用户对注册登录的用户的权限进行更改，虽为User用户，但拥有最大权限，检查数据库，数据库信息正常。      可能是数据库的Admin用户更改用户权限时，其中的role与Role未关联。即没有设置外键。下一提交测试。     <2>数据库在昨天曾经为了清空数据库信息，返回最后一次更新后的某一版本。之后曾因为用户权限问题曾多次清空重建。在重新更新数据库，创建迁移脚本文件时提示alembic.util.exc.CommandError: Target database is not up to date. 即旧版本优于新版本，不能更新。最后是删除全部之前的更新文件才解决，作为最新以及唯一，不需更新    <3>本次提交的关键在于结联两个数据库。

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
